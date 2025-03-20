6f184d8994336111f65f842995554830eec51fdf
Dominik Machowski <dominik@Dominiks-Laptop.local>
2025-03-20 20:21:14 +0100

    Netlify package for SSR added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,15 @@ import tailwindcss from "@tailwindcss/vite"
 
 import react from '@astrojs/react';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: netlify()
 });
